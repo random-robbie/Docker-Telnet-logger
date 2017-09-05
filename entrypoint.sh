2d9a814abf4a9bde3e00d486bc2d0d60dc2153a8
Robbie <txt3rob@gmail.com>
2017-09-05 13:37:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
-nohup /telnetlogger/telnetlogger -p passwds.txt -i ips.txt &
+screen -dms telnet "/telnetlogger/telnetlogger -p passwds.txt -i ips.txt"
 echo "Telnet Honeypot is Running..."
 exec "$@"
